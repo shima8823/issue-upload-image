643c972bc40bdd48027329c9af19161be70592cc
shima8823 <taiyaki1275@gmail.com>
2024-04-29 14:26:24 +0900

    feat: upload files

--- frontend_react/src/App.tsx
+++ frontend_react/src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const FileUploader = () => {
+  const [file, setFile] = useState(null);
+  const [uploadStatus, setUploadStatus] = useState('');
+
+  const handleFileChange = (event: any) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleUpload = async () => {
+    if (!file) {
+      setUploadStatus('ファイルが選択されていません');
+      return;
+    }
+
+    setUploadStatus('アップロード中...');
+
+    try {
+      const formData = new FormData();
+      formData.append('file', file);
+
+      const response = await fetch('http://localhost:3000/upload', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (response.ok) {
+        setUploadStatus('アップロード完了');
+        console.log(await response.json());
+      } else {
+        setUploadStatus('アップロードに失敗しました');
+        console.error('エラー:', response.status);
+      }
+    } catch (error) {
+      setUploadStatus('アップロードに失敗しました');
+      console.error('エラー:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      <button onClick={handleUpload}>アップロード</button>
+      <p>{uploadStatus}</p>
     </div>
   );
-}
+};
 
-export default App;
+export default FileUploader;
