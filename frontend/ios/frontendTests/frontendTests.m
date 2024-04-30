26683cb00cdc57cd3acc341be42206f5a3f3724a
shima8823 <taiyaki1275@gmail.com>
2024-04-30 21:38:12 +0900

    chore: rename directory

--- frontend/ios/frontendTests/frontendTests.m
+++ frontend/ios/frontendTests/frontendTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface frontendreactnativeTests : XCTestCase
+@interface frontendTests : XCTestCase
 
 @end
 
-@implementation frontendreactnativeTests
+@implementation frontendTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
